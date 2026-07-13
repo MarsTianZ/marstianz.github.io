b49c8feeae6e052d79b71bbdb894dddc895c2fae
MarsTianZ <marsa.kristian66@gmail.com>
2026-07-13 12:58:29 +0700

    updated Home Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -121,11 +121,11 @@ export default function Home({ setPreviewData }) {
         >
           <div className="w-full bg-gray-900/50 p-6 md:p-10 rounded-2xl animate-fade-in backdrop-blur-sm relative z-10 text-justify">
             <p className="text-gray-100 leading-relaxed text-xl font-medium ">
-              Hello! I’m a recent Information Systems graduate with a strong
-              interest in technology. My interests range from user experience
-              (UI/UX) design to the technical aspects of operating systems. I’m
-              always up for the challenge of meeting user needs with functional
-              digital solutions.
+              Hello! I'm an Information Systems student awaiting my formal
+              graduation in August 2026 with a strong interest in technology.
+              My interests range from user experience (UI/UX) design to the
+              technical aspects of operating systems. I’m always up for the
+              challenge of meeting user needs with functional digital solutions.
               <br />
               <br />
               My primary focus is UI/UX design and web development. I solve
